7ceb39b007ec02b501d50f4a0c0c29e39c3ab796
tothasz <albert.szilard@sonrisa.hu>
2024-05-01 08:58:42 +0300

    First commit

---
+++ src/test/java/com/sapientia/aihealth/AiHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapientia.aihealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
