f67d4ef3009e02c29b2c5037e8b0b92ab2ff2602
Albert-Toth <aszilard99@gmail.com>
2024-05-04 20:47:27 +0300

    WIP searching for the reason of the java vs python inference discrepancy

--- src/main/java/com/sapientia/aihealth/controllers/ClassificationController.java
+++ src/main/java/com/sapientia/aihealth/controllers/ClassificationController.java
@@ -1,5 +1,6 @@
 package com.sapientia.aihealth.controllers;
 
+import ai.djl.modality.Classifications;
 import ai.djl.modality.cv.Image;
 import com.sapientia.aihealth.ai.classification.InferenceService;
 import com.sapientia.aihealth.ai.preprocessing.ImagePreprocessingService;
@@ -20,34 +21,28 @@ public class ClassificationController {
     @PostMapping("/upload")
     public boolean classify(MultipartFile image){
         boolean res = false;
-
         try {
-
             ImagePreprocessingService imgPreprocessor = new ImagePreprocessingService();
             ImageTypeConverter imageTypeConverter = new ImageTypeConverter();
 
             Mat matImage = imageTypeConverter.byteArrayToMat(image.getBytes());
 
-            matImage = imgPreprocessor.cropBrainContour(matImage);
-            //res = ImageIO.write(imageTypeConverter.matToBufferedImage(matImage), "jpg", new File("./processedImg.jpg"));
-            imgPreprocessor.resizeImage(matImage, 240, 240);
-            // = ImageIO.write(imageTypeConverter.matToBufferedImage(matImage), "jpg", new File("./processedResizedImg.jpg"));
-            Mat normalizedMatImage = imgPreprocessor.normalizeImage(matImage);
+            //matImage = imgPreprocessor.cropBrainContour(matImage);
+            //imgPreprocessor.resizeImage(matImage, 240, 240);
+            //Mat normalizedMatImage = imgPreprocessor.normalizeImage(matImage);
             //System.out.println("Normalized value in controller at (100,100): " + normalizedMatImage.get(100, 100)[0]);
 
-            Image djlImage = imageTypeConverter.matToDJLImage(normalizedMatImage);
-
-            InferenceService.predict(djlImage);
-
+            Image djlImage = imageTypeConverter.matToDJLImage(matImage);
+            Classifications result = InferenceService.predict(djlImage);
+            System.out.println(result.getProbabilities().toString());
             /*//for debugging
             //for some reason after normalizing, the ImageIO.write function cant create a jpg from the mat file
             BufferedImage bufferedImage = imageTypeConverter.matToBufferedImage(normalizedMatImage);
-            res = ImageIO.write(bufferedImage, "jpg", new File("./processedNormalizedImg.jpg"));*/
-
+            res = ImageIO.write(bufferedImage, "jpg", new File("./processedNormalizedImg.jpg"));
+            System.out.println("Normalized value in controller at (100,100): " + normalizedMatImage.get(100, 100)[0]);*/
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return res;
     }
 }
--- src/main/java/com/sapientia/aihealth/controllers/HelloController.java
+++ src/main/java/com/sapientia/aihealth/controllers/HelloController.java
@@ -10,25 +10,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 @RestController
 public class HelloController {
     @GetMapping("/")
     public Double index() {
         Classifications result;
+        Classifications result2;
         double probability = 0;
         try {
-            //Image img2 = ImageFactory.getInstance().fromFile(Paths.get("N48.jpeg"));
+            Image img2 = ImageFactory.getInstance().fromFile(Paths.get("aug_N16_0_9930_python_processed.jpg"));
+            result2 = InferenceService.predict(img2);
+            System.out.println(" result2 djl Image " + result2.getProbabilities());
 
-            BufferedImage bufferedImage = CustomImageReader.loadImage("Y103.jpg");
+            BufferedImage bufferedImage = CustomImageReader.loadImage("aug_N16_0_9930_python_processed.jpg");
 
             Image img = ImageFactory.getInstance().fromImage(bufferedImage);
 
 
             result = InferenceService.predict(img);
             probability = result.getProbabilities().get(0);
-            System.out.println(" result " + probability);
-        } catch (TranslateException e) {
+            System.out.println(" result " + result.getProbabilities());
+        } catch (TranslateException | IOException e) {
             e.printStackTrace();
         }
         return probability;
