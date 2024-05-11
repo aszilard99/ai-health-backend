ddd8e3202c3e3aa616b5ad80c40169d6fa9d0658
Albert-Toth <aszilard99@gmail.com>
2024-05-11 16:11:37 +0300

    Set up cors policy

--- src/main/java/com/sapientia/aihealth/controllers/ClassificationController.java
+++ src/main/java/com/sapientia/aihealth/controllers/ClassificationController.java
@@ -2,6 +2,7 @@ package com.sapientia.aihealth.controllers;
 
 import com.sapientia.aihealth.services.ClassificationService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,9 +12,12 @@ import java.io.IOException;
 @RestController
 public class ClassificationController {
 
+    //TODO the crossOrigin config was from the video: https://www.youtube.com/watch?v=Ly79YDERpas
+    //TODO when dockerizing the app, might need to remove the port from this url
     @PostMapping("/classify")
-    public String classify(MultipartFile image) {
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<String> classify(MultipartFile image) {
         ClassificationService classificationService = new ClassificationService();
-        return classificationService.classify(image).getBody();
+        return classificationService.classify(image);
     }
 }
